a467ecf0b1a811f669a9c420d568ac8450410e08
Siim Rebane <siim@rebane.org>
2020-08-24 11:50:45 +0300

    First lesson

---
+++ src/Lesson1.java
@@ -0,0 +1,42 @@
+public class Lesson1 {
+
+    public static void main(String[] args) {
+        excersie1();
+        excersie2();
+        excersie3();
+        excersie4();
+    }
+
+    public static void excersie1(){
+        // TODO
+        // defineeri 3 muutujat a = 1, b = 1, c = 3
+        // Prindi välja a==b ja a==c
+        // Lisa rida a = c
+        // Prindi välja a==b ja a==c, mis muutus???
+    }
+
+    public static void excersie2(){
+        // TODO
+        // Loo muutujad x1 = 10 ja x2 = 20, vali sobiv andmetüüp
+        // Tekita muutuja y1 = ++x1, trüki välja nii x1 kui y1
+        // Tekita muutuja y2 = x2++, trüki välja nii x2 ja y2
+        // Analüüsi tulemusi
+    }
+
+    public static void excersie3(){
+        // TODO
+        // Loo arvulised muutujad
+        // a = 18 % 3
+        // b = 19 % 3
+        // c = 20 % 3
+        // d = 21 % 3
+        // Prindi välja kõigi muutujate väärtused
+    }
+
+    public static void excersie4(){
+        // TODO
+        // Defineeri String tüüpi muutuja mille sisu oleks „\“\\““
+        // Trüki muutuja sisu välja
+    }
+
+}
---
+++ src/Lesson1MathUtil.java
@@ -0,0 +1,38 @@
+public class Lesson1MathUtil {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int a, int b){
+        // TODO tagasta a ja b väikseim väärtus
+        return 0;
+    }
+
+    public static int max(int a, int b){
+        // TODO tagasta a ja b suurim väärtus
+        return 0;
+    }
+
+    public static int abs(int a){
+        // TODO tagasta a absoluut arv
+        return 0;
+    }
+
+    public static boolean isEven(int a){
+        // TODO tagasta true, kui a on paaris arv
+        // tagasta false kui a on paaritu arv
+        return false;
+    }
+
+    public static int min(int a, int b, int c){
+        // TODO tagasta a, b ja c väikseim väärtus
+        return 0;
+    }
+
+    public static int max(int a, int b, int c){
+        // TODO tagasta a, b ja c suurim väärtus
+        return 0;
+    }
+
+}
