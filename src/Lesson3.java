da16191f17994d86a18031e9c5034b929c1afd65
Siim Rebane <siim@rebane.org>
2020-08-26 01:55:25 +0300

    Lesson3

---
+++ src/Lesson3.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+public class Lesson3 {
+
+    public static void main(String[] args) {
+    }
+
+    public static int sum(int x, int y) {
+        // TODO liida kokku ja tagasta x ja y väärtus
+        return 0;
+    }
+
+    public static int sum(int[] x){
+        // Todo liida kokku kõik numbrid massivis x
+        return 0;
+    }
+
+    public static int factorial(int x) {
+        // TODO tagasta x faktoriaal.
+        // Näiteks
+        // x = 5
+        // return 4*3*2*1 = 24
+        return 0;
+    }
+
+    public static int[] sort(int[] a) {
+        // TODO sorteeri massiiv suuruse järgi
+        // Näiteks {2, 6, 8, 1}
+        // Väljund {1, 2, 6, 8}
+        return new int[0];
+    }
+
+    public static String reverseString(String a) {
+        // TODO tagasta string tagurpidi
+        // Näiteks:
+        // a = "Test";
+        // return tseT";
+        return "";
+    }
+
+    public static boolean isPrime(int x){
+        // TODO tagasta kas sisestatud arv on primaar arv (jagub ainult 1 ja iseendaga)
+        return false;
+    }
+
+}
---
+++ src/Lesson3Hard.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+// Enne kui seda tegema hakkad tee ära Lesson 2 (välja arvatud ülesanded 6, 8, 9)
+public class Lesson3Hard {
+    public static void main(String[] args) {
+
+    }
+
+    public static int evenFibonacci(int x){
+        // TODO liida kokku kõik paaris fibonacci arvud kuni numbrini x
+        return 0;
+    }
+
+    public static void randomGame(){
+        // TODO kirjuta mäng mis võtab suvalise numbri 0-100, mille kasutaja peab ära arvama
+        // iga kord pärast kasutaja sisestatud täis arvu peab programm ütlema kas number oli suurem või väiksem
+        // ja kasutaja peab saama uuesti arvata
+        // numbri ära aramise korral peab programm välja trükkima mitu katset läks numbri ära arvamiseks
+        Random random = new Random();
+        int i = random.nextInt(100);
+        System.out.println(i);
+    }
+
+    public static String morseCode(String text){
+        // TODO kirjuta programm, mis tagastab sisestatud teksti morse koodis (https://en.wikipedia.org/wiki/Morse_code)
+        // Kasuta sümboleid . ja -
+        return "";
+    }
+}
