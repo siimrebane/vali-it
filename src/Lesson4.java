f6403c37b915544e23e053dd1a3b95a732a1e388
Siim Rebane <siim@rebane.org>
2020-09-01 10:49:26 +0300

    vali it

--- src/Lesson4.java
+++
@@ -1,9 +0,0 @@
-public class Lesson4 {
-    // TODO loo programm, mis hoiab mälus panga konto numbrit ja konto balanssi
-    // TEE programm:
-    //
-    public static void main(String[] args) {
-
-    }
-
-}
