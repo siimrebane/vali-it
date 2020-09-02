6de23258dd3cf179e51438ade2f66f1cdc062372
Siim Rebane <siim@rebane.org>
2020-09-02 12:54:48 +0300

    Slaidid 7

--- src/main/java/ee/bcs/valiit/controller/TestController.java
+++ src/main/java/ee/bcs/valiit/controller/TestController.java
@@ -1,5 +1,7 @@
 package ee.bcs.valiit.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
@@ -11,8 +13,28 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
     private static final List<BankAccount> bankAccounts = new ArrayList();
     private static final Map<String, BankAccount> bankAccounts2 = new HashMap<>();
+
+    @GetMapping("sqltest")
+    public String testSql(){
+        String sql = "SELECT account_nr FROM account where id = :id";
+        Map<String, Object> paramMap = new HashMap();
+        paramMap.put("id", 1);
+        String vastus = jdbcTemplate.queryForObject(sql, paramMap, String.class);
+
+        sql = "UPDATE account SET balane = :balance where id = :id";
+        paramMap = new HashMap<>();
+        paramMap.put("balance", 100);
+        paramMap.put("id", 2);
+        jdbcTemplate.update(sql, paramMap);
+
+        return vastus;
+    }
+
 
     @PostMapping("test/{id1}/{id2}/{id3}")
     public List<BankAccount> test(@PathVariable("id1") String pathId1,
--- src/main/java/ee/bcs/valiit/old/Lesson4.java
+++ src/main/java/ee/bcs/valiit/old/Lesson4.java
@@ -1,11 +1,20 @@
 package ee.bcs.valiit.old;
 
+import java.math.BigDecimal;
+
 public class Lesson4 {
     // TODO loo programm, mis hoiab mälus panga konto numbrit ja konto balanssi
     // TEE programm:
     //
     public static void main(String[] args) {
-
+        BigDecimal a = new BigDecimal("10");
+        BigDecimal b = BigDecimal.ONE;
+        
+        if(a.compareTo(b) < 0){
+            System.out.println("jep");
+        } else {
+            System.out.println("ei ole");
+        }
     }
 
 }
