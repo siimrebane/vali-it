127d47aaa9264f1a0d721503cd148db9d67cb760
Siim Rebane <siim@rebane.org>
2021-01-12 09:01:46 +0200

    uued ülesandede

---
+++ src/main/java/ee/bcs/valiit/tasks/Lesson3.java
@@ -0,0 +1,36 @@
+package ee.bcs.valiit.tasks;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int sum(int[] x){
+        // Todo liida kokku kõik numbrid massivis x
+        return 0;
+    }
+
+    public static int factorial(int x) {
+        // TODO tagasta x faktoriaal.
+        // Näiteks
+        // x = 5
+        // return 5*4*3*2*1 = 120
+        return 0;
+    }
+
+    public static int[] sort(int[] a) {
+        // TODO sorteeri massiiv suuruse järgi.
+        // TODO kasuta tsükleid, ära kasuta ühtegi olemasolevat sort funktsiooni
+        return new int[0];
+    }
+
+    public static String reverseString(String a) {
+        // TODO tagasta string tagurpidi
+        return "";
+    }
+
+    public static boolean isPrime(int x){
+        // TODO tagasta kas sisestatud arv on primaar arv (jagub ainult 1 ja iseendaga)
+        return false;
+    }
+}
