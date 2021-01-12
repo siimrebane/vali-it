48e7f1011a53bdd7981838d8169fa01f5ed6736c
Siim Rebane <siim@rebane.org>
2021-01-12 16:51:01 +0200

    lesson2 lahendus

--- src/main/java/ee/bcs/valiit/tasks/solution/Lesson2.java
+++ src/main/java/ee/bcs/valiit/tasks/solution/Lesson2.java
@@ -1,13 +1,11 @@
 package ee.bcs.valiit.tasks.solution;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.*;
+import java.util.Scanner;
 
 public class Lesson2 {
 
     public static void main(String[] args) {
-        exercise1();
+        System.out.println(exercise5(1, 10));
     }
 
     // TODO loo 10 elemendile täisarvude massiv
@@ -16,11 +14,11 @@ public class Lesson2 {
     public static void exercise1() {
         int[] array = new int[10];
         Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.println("Sisesta number: ");
             array[i] = scanner.nextInt();
         }
-        for(int i = 9; i >= 0; i--){
+        for (int i = 9; i >= 0; i--) {
             System.out.print(array[i] + " ");
         }
     }
@@ -30,6 +28,9 @@ public class Lesson2 {
     // Sisend 5
     // Väljund 2 4 6 8 10
     public static void exercise2(int x) {
+        for (int i = 1; i <= x; i++) {
+            System.out.print(i * 2 + " ");
+        }
     }
 
     // TODO trüki välja korrutustabel mis on x ühikut lai ja y ühikut kõrge
@@ -46,7 +47,12 @@ public class Lesson2 {
     // TODO 5 võrdle ridu. Kas on mingi seaduspärasus ridade vahel,
     // mis on ja mis võiks olla. Äkki tuleb mõni idee
     public static void exercise3(int x, int y) {
-
+        for (int j = 1; j <= y; j++) {
+            for (int i = 1; i <= x; i++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
     }
 
     // TODO
@@ -54,16 +60,54 @@ public class Lesson2 {
     // 0, 1, 1, 2, 3, 5, 8, 13, 21
     // Tagasta fibonacci jada n element
     public static int exercise4(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            int a = 0;
+            int b = 1;
+            for (int i = 1; i < n; i++) {
+                int tmp = a;
+                a = b;
+                b = b + tmp;
+            }
+            return b;
+        }
+    }
 
-        return 0;
+    public static int exercise4Recursion(int n) {
+        if(n <= 0){
+            return 0;
+        } else if (n == 1){
+            return 1;
+        } else {
+            return exercise4Recursion(n-1) + exercise4Recursion(n-2);
+        }
     }
 
     public static String exercise5(int x, int y) {
-        // https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=36
-        // TODO 1 (tee alamfunktsioon) mis leiab 3n+1 sequenci pikkuse
-        // kui on paaris / 2 kui on paaritu *3+1
-        // TODO 2 tee tsükkel mis leiab i -> j kõige suurema tsükkli pikkuse
-        return "";
+        int maxLength = 0;
+        for(int i = x; i <= y; i++){
+            int seqLength = seqLength(i);
+            if(seqLength > maxLength){
+                maxLength = seqLength;
+            }
+        }
+        return x + " " + y + " " + maxLength;
+    }
+    
+    public static int seqLength(int n) {
+        int count = 1;
+        while(n > 1){
+            count++;
+            if(n%2 == 0){
+                n = n / 2;
+            } else {
+                n = n*3 + 1;
+            }
+        }
+        return count;
     }
 
 
