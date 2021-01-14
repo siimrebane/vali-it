b6d7b77cfc5af9ce6ed11bc0fa370b8870522ae9
Siim Rebane <siim@rebane.org>
2021-01-14 16:06:04 +0200

    sort solution 2

--- src/main/java/ee/bcs/valiit/tasks/solution/Lesson3.java
+++ src/main/java/ee/bcs/valiit/tasks/solution/Lesson3.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Lesson3 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sort(new int[] {5, -6, 9, 12, 7})));
+        System.out.println(Arrays.toString(sort2(new int[]{5, -6, 9, 12, 7})));
     }
 
-    public static int sum(int[] x){
+    public static int sum(int[] x) {
         int sum = 0;
         for (int i : x) {
             sum += i;
@@ -17,7 +17,7 @@ public class Lesson3 {
 
     public static int factorial(int x) {
         int sum = 1;
-        for(int i = 1; i < x; i++){
+        for (int i = 1; i < x; i++) {
             sum *= i;
         }
         return sum;
@@ -25,16 +25,32 @@ public class Lesson3 {
 
     // 5, -6, 9, 12, 7
     public static int[] sort(int[] a) {
-        for(int j = 0; j < a.length ;j++){
-            for(int i = 0; i < a.length -1;i++){
-                if(a[i] > a[i+1]){
+        for (int j = 0; j < a.length; j++) {
+            for (int i = 0; i < a.length - 1 - j; i++) {
+                if (a[i] > a[i + 1]) {
                     int tmp = a[i];
-                    a[i] = a[i+1];
-                    a[i+1] = tmp;
+                    a[i] = a[i + 1];
+                    a[i + 1] = tmp;
                 }
             }
         }
         return a;
+    }
+
+    // 5, -6, 9, 12, 7
+    public static int[] sort2(int[] array) {
+        for (int j = 0; j < array.length - 1; j++) {
+            int minIndex = j;
+            for (int i = j + 1; i < array.length; i++) {
+                if (array[i] < array[minIndex]) {
+                    minIndex = i;
+                }
+            }
+            int tmp = array[j];
+            array[j] = array[minIndex];
+            array[minIndex] = tmp;
+        }
+        return array;
     }
 
     public static String reverseString(String a) {
@@ -42,7 +58,7 @@ public class Lesson3 {
         return "";
     }
 
-    public static boolean isPrime(int x){
+    public static boolean isPrime(int x) {
         // TODO tagasta kas sisestatud arv on primaar arv (jagub ainult 1 ja iseendaga)
         return false;
     }
