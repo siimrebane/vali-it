0e7af228e232b2e6b77dc585920b728629b6c999
Siim Rebane <siim@rebane.org>
2021-01-15 04:30:52 +0200

    Lesson 4

--- src/main/java/ee/bcs/valiit/tasks/Lesson4.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson4.java
@@ -1,4 +1,43 @@
 package ee.bcs.valiit.tasks;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Lesson4 {
+    // Store account nr as a key and account balance as value
+    HashMap<String, BigDecimal> accountBalanceMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String line = scanner.nextLine();
+            if (line.equalsIgnoreCase("exit")){
+                break;
+            }
+            // TODO 1
+            // Add command: "createAccount ${accountNr}"
+            // this has to store accountNr with 0 balance
+            // TODO 2
+            // Add command: "getBalance ${accountNr}"
+            // this has to display account balance of specific acount
+            // TODO 3
+            // Add command: "depositMoney ${accountNr} ${amount}
+            // this has to add specified amount of money to account
+            // You have to check that amount is positive number
+            // TODO 4
+            // Add command: "withdrawMoney ${accountNr} ${amount}
+            // This has to remove specified amount of money from account
+            // You have to check that amount is positive number
+            // You may not allow this transaction if account balance would become negative
+            // TODO 5
+            // Add command: "transfer ${fromAccount} ${toAccount} ${amount}
+            // This has to remove specified amount from fromAccount and add it to toAccount
+            // Your application needs to check that toAccount is positive
+            // And from account has enough money to do that transaction
+            else {
+                System.out.println("Unknown command");
+            }
+        }
+    }
 }
