d5b306b74bea40d0d046c39f8d93ad7e18fdac33
Siim Rebane <siim@rebane.org>
2021-01-18 14:45:57 +0200

    controller examples

--- src/main/java/ee/bcs/valiit/tasks/solution/controller/SolutionTestController.java
+++ src/main/java/ee/bcs/valiit/tasks/solution/controller/SolutionTestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SolutionTestController {
 
+    // localhost:8080/solution/test/siim
     @GetMapping("test/{name}")
     public String getHelloWorld(@PathVariable("name") String userName, @RequestParam("name") String lastName){
         return "Hello " + userName + " " + lastName;
