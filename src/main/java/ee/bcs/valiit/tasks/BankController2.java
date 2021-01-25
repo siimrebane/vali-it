dd53d7246cec2dbe162b9464053c33c1783d30ea
Siim Rebane <siim@rebane.org>
2021-01-25 13:57:20 +0200

    Bank2 refacto

--- src/main/java/ee/bcs/valiit/tasks/BankController2.java
+++ src/main/java/ee/bcs/valiit/tasks/BankController2.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-@RequestMapping("solution/bank")
+@RequestMapping("bank2")
 @RestController
 public class BankController2 {
 
