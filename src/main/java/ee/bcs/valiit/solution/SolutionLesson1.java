3ca37df45077dfbf5495db9a5232da639eaca1e1
Siim Rebane <siim@rebane.org>
2021-04-05 15:06:34 +0300

    lahendused

---
+++ src/main/java/ee/bcs/valiit/solution/SolutionLesson1.java
@@ -0,0 +1,62 @@
+package ee.bcs.valiit.solution;
+
+// TODO kasuta if/else. Ära kasuta Math librarit
+public class SolutionLesson1 {
+    public static void main(String[] args) {
+        // Siia sisse võid sa kirjutada koodi, et testida kas su meetodid töötavad ilusti
+        // Katseta IntelliJ shortcuti. Olles intelliJ kirjuta "sout" ja siis vajuta enter
+        System.out.println(min(1, 3)); // trükib miinimumi 1 ja 3
+    }
+
+    // TODO tagasta a ja b väikseim väärtus
+    public static int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    // TODO tagasta a ja b suurim väärtus
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    // TODO tagasta a absoluut arv
+    public static int abs(int a) {
+        if (a < 0) {
+            return -a;
+        } else {
+            return a;
+        }
+    }
+
+    // TODO tagasta true, kui a on paaris arv
+    // tagasta false kui a on paaritu arv
+    public static boolean isEven(int a) {
+        if (a % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    // TODO tagasta kolmest arvust kõige väiksem
+    public static int min3(int a, int b, int c) {
+        if(a <= b && a <= c){
+            return a;
+        } else if(b <= a && b <= c){
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    // TODO tagasta kolmest arvust kõige suurem
+    public static int max3(int a, int b, int c) {
+        return max(max(a, b), c);
+    }
+}
