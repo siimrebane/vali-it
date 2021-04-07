da22c8425f07c9017f0689109cfea37b41ca4194
Siim Rebane <siim@rebane.org>
2021-04-07 08:42:51 +0300

    Lesson2b

--- src/main/java/ee/bcs/valiit/tasks/Lesson2b.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson2b.java
@@ -25,4 +25,20 @@ public class Lesson2b {
     public static int[] generateArray(int n){
         return new int[0];
     }
+
+    // TODO
+    // Tagastada massiiv kus oleks numbrid n-ist 0 ini
+    // Näiteks: sisend: 5
+    // Väljund: 5, 4, 3, 2, 1, 0
+    // Näide2: siend: -3
+    // Väljund: -3, -2, -1, 0
+    public static int[] decreasingArray(int n){
+        return new int[0];
+    }
+
+    // TODO
+    // tagasta massiiv pikkusega n, mille kõigi elementide väärtused on 3
+    public static int[] yl3(int n){
+        return new int[0];
+    }
 }
