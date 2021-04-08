a3a51e799081e9ae754ea5169878d8449750569f
Siim Rebane <siim@rebane.org>
2021-04-08 12:08:53 +0300

    list and map sample

---
+++ src/test/java/ee/bcs/valiit/tasks/Lesson2cTest.java
@@ -0,0 +1,33 @@
+package ee.bcs.valiit.tasks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Lesson2cTest {
+
+    @Test
+    void getSeqLength() {
+        Assertions.assertEquals(1, Lesson2c.getSeqLength(1));
+        Assertions.assertEquals(6, Lesson2c.nextElement(5));
+        Assertions.assertEquals(4, Lesson2c.nextElement(3));
+        Assertions.assertEquals(3, Lesson2c.nextElement(8));
+    }
+
+    @Test
+    void nextElement() {
+        Assertions.assertEquals(1, Lesson2c.nextElement(2));
+        Assertions.assertEquals(10, Lesson2c.nextElement(3));
+        Assertions.assertEquals(5, Lesson2c.nextElement(10));
+        Assertions.assertEquals(16, Lesson2c.nextElement(15));
+    }
+
+    @Test
+    void sequence3n() {
+        Assertions.assertEquals(20, Lesson2c.sequence3n(1, 10));
+        Assertions.assertEquals(125, Lesson2c.sequence3n(100, 200));
+        Assertions.assertEquals(89, Lesson2c.sequence3n(201, 210));
+        Assertions.assertEquals(174, Lesson2c.sequence3n(900, 1000));
+    }
+}
