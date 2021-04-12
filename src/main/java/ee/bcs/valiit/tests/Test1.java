ea822594c7869983bc35497b474c8b1dc14414f8
Siim Rebane <siim@rebane.org>
2021-04-12 09:42:30 +0300

    test1

---
+++ src/main/java/ee/bcs/valiit/tests/Test1.java
@@ -0,0 +1,22 @@
+package ee.bcs.valiit.tests;
+
+public class Test1 {
+    public static void main(String[] args) {
+    }
+//    ÜL 1
+//    Tee funktsioon, mis tagastab boolean väärtuse ja võtab sisse ühe parameetri
+//    funktsioon peab tagastama
+//		true: kui sisend parameeter jagub 3 või 7 (aga ei jagu mõlema numbriga)
+//            false: kui sisend parameeter ei jagu 3 ega 7 või jagub mõlema numbriga
+
+
+    // ÜL2
+    // lisa x igale array elemendile
+    // Näiteks
+    // sisend [1,2,3], 5
+    // vastus [6,7,8]
+    public static int[] addToArray(int[] array, int x){
+        return new int[0];
+    }
+
+}
