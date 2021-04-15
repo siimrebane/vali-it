17efcf27897a16d7ba6703bcf5b3a8767a366ff2
Siim Rebane <siim@rebane.org>
2021-04-15 15:31:32 +0300

    Lesson 5

---
+++ src/main/java/ee/bcs/valiit/Circle.java
@@ -0,0 +1,20 @@
+package ee.bcs.valiit;
+
+public class Circle extends Shape {
+    int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public double circumference(){
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double area(){
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
---
+++ src/main/java/ee/bcs/valiit/tasks/Lesson5.java
@@ -0,0 +1,18 @@
+package ee.bcs.valiit.tasks;
+
+public class Lesson5 {
+    // TODO täienda oma BankControllerit nii, et sa hoiad Map-is konto balanssi asemel konto objekti
+    //  1)
+    // Selleks loo uus konto objekt, mis sisaldab minimaalselt
+    // * kontoNr
+    // * kontoOmanikuNimi
+    // * balanss
+    // * kas konto on lukustatud
+    // 2)
+    // täienda konto loomis teenust nii, et sa salvestad ka konto omaniku nime
+    // 3)
+    // loo 2 uut teenust: lockAccount ja unlockAccount
+    // 4)
+    // juhul kui konto on lukustatud ei tohi saada sellele kontole raha juurde kande ega kontolt raha ära võtta
+    // ehk siis withdrawMoney, depositMoney ja transferMoney teenused ei tohi töötada
+}
