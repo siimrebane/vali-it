e18b9bceec2582c5830a32972c6ecd70ee4132bf
Siim Rebane <siim@rebane.org>
2021-04-21 15:36:28 +0300

    throw sample

--- src/main/java/ee/bcs/valiit/solution/service/SampleBankService.java
+++ src/main/java/ee/bcs/valiit/solution/service/SampleBankService.java
@@ -1,10 +1,15 @@
 package ee.bcs.valiit.solution.service;
 
+import ee.bcs.valiit.solution.controller.SampleAccount2;
+import ee.bcs.valiit.solution.controller.SampleAccount2RowMapper;
+import ee.bcs.valiit.solution.exception.SampleApplicationException;
 import ee.bcs.valiit.solution.repository.SampleAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -12,8 +17,23 @@ public class SampleBankService {
     @Autowired
     private SampleAccountRepository accountRepository;
 
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
     public void createAccount(String accountNr, Double balance) {
+        if(balance < 0){
+            throw new SampleApplicationException("Summa ei tohi olla negatiivne");
+        }
         accountRepository.createAccount(accountNr, balance);
+    }
+
+    public String getBalance(String accountNr){
+        Double balance = accountRepository.getBalance(accountNr);
+        return "Konto balanss on: " + balance;
+    }
+
+    public List<SampleAccount2> getAllAccounts() {
+        String sql = "SELECT * FROM account";
+        return jdbcTemplate.query(sql, new HashMap(), new SampleAccount2RowMapper());
     }
 }
