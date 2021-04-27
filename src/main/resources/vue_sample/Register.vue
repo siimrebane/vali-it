d63177d019feb585bc4266c20240bf9df2e0816e
Siim Rebane <siim@rebane.org>
2021-04-27 14:05:49 +0300

    vue js

--- src/main/resources/vue_sample/Register.vue
+++ src/main/resources/vue_sample/Register.vue
@@ -1,54 +1,21 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        Name: <input v-model="employee.name" placeholder="first name">
-        Address: <input v-model="employee.address" placeholder="last name">
-        <button v-on:click="saveInHtml()">Salvesta</button>
-        <table border="1">
-            <tr>
-                <th>id</th>
-                <th>name</th>
-                <th>address</th>
-                <th>phone</th>
-            </tr>
-            <tr v-for="row in employees">
-                <td>{{row.id}}</td>
-                <td>{{row.name}}</td>
-                <td>{{row.address}}</td>
-                <td>{{row.phone}}</td>
-            </tr>
-        </table>
-    </div>
+  <div>
+    <input v-model="email" placeholder="enter your email"/>
+    <button v-on:click="register()">Register</button>
+  </div>
 </template>
-
 <script>
-    let getData = function () {
-        this.$http.get('http://localhost:8080/solution/employee')
-            .then(response => this.employees = response.data)
-            .catch(response => console.log(response))
-    }
 
-    // @ is an alias to /src
-    let saveInJs = function () {
-        this.$http.post('http://localhost:8080/solution/employee/test', this.employee)
-        .then(() => this.getData());
+export default {
+  data: function (){
+    return {
+      'email': ''
     }
-
-    export default {
-        name: 'Register',
-        components: {},
-        data: function () {
-            return {
-                employee: {},
-                employees: []
-            }
-        },
-        methods: {
-            saveInHtml: saveInJs,
-            getData: getData
-        },
-        mounted() {
-            this.getData();
-        }
+  },
+  methods: {
+    'register': function (){
+      alert(this.email);
     }
+  }
+}
 </script>
