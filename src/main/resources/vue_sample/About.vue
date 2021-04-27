99f85ccf047e1b704c3f3c69d82e3ef0dac05316
Siim Rebane <siim@rebane.org>
2021-04-27 14:12:04 +0300

    vue js

---
+++ src/main/resources/vue_sample/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>Min</h1>
+    <input v-model.number="minA">
+    <input v-model.number="minB">
+    <button v-on:click="min()">Min</button>
+    {{minAnswer}}
+  </div>
+</template>
+
+<script>
+
+function min(){
+  if(this.minA < this.minB){
+    this.minAnswer = this.minA;
+  } else {
+    this.minAnswer = this.minB;
+  }
+}
+
+export default {
+  data: function (){
+    return {
+      'minA': 0,
+      'minB': 0,
+      'minAnswer': 0
+    }
+  },
+  methods: {
+    'min': min
+  }
+}
+</script>
