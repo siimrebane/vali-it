d2324d1e2bcc25a9242878d81e04bf4703e89eb0
Siim Rebane <siim@rebane.org>
2021-04-28 14:42:04 +0300

    rowmapper sample

--- src/main/java/ee/bcs/valiit/solution/service/SampleBankService.java
+++ src/main/java/ee/bcs/valiit/solution/service/SampleBankService.java
@@ -1,8 +1,10 @@
 package ee.bcs.valiit.solution.service;
 
 import ee.bcs.valiit.solution.controller.SampleAccount2;
-import ee.bcs.valiit.solution.controller.SampleAccount2RowMapper;
+import ee.bcs.valiit.solution.repository.SampleAccount2RowMapper;
 import ee.bcs.valiit.solution.exception.SampleApplicationException;
+import ee.bcs.valiit.solution.hibernate.SampleAccount;
+import ee.bcs.valiit.solution.hibernate.SampleHibernateAccountRepository;
 import ee.bcs.valiit.solution.repository.SampleAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -10,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class SampleBankService {
@@ -20,16 +21,21 @@ public class SampleBankService {
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private SampleHibernateAccountRepository hibernateRepository;
+
     public void createAccount(String accountNr, Double balance) {
-        if(balance < 0){
+        if (balance < 0) {
             throw new SampleApplicationException("Summa ei tohi olla negatiivne");
         }
         accountRepository.createAccount(accountNr, balance);
     }
 
-    public String getBalance(String accountNr){
-        Double balance = accountRepository.getBalance(accountNr);
-        return "Konto balanss on: " + balance;
+    public String getBalance(String accountNr) {
+        SampleAccount account = hibernateRepository.getOne(accountNr);
+        return "Konto balanss on: " + account.getBalance();
+        //Double balance = accountRepository.getBalance(accountNr);
+        //return "Konto balanss on: " + balance;
     }
 
     public List<SampleAccount2> getAllAccounts() {
