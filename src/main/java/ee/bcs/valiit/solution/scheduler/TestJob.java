4769d09615a87762b47cbcf4141c7fb210f25e9e
Siim Rebane <siim@rebane.org>
2021-05-06 13:50:46 +0300

    send email

--- src/main/java/ee/bcs/valiit/solution/scheduler/TestJob.java
+++ src/main/java/ee/bcs/valiit/solution/scheduler/TestJob.java
@@ -1,17 +1,40 @@
 package ee.bcs.valiit.solution.scheduler;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 @Component
 public class TestJob {
 
 
     @Scheduled(fixedDelay = 1000)
-    public void test(){
-        System.out.println("läks tööle" + LocalDateTime.now());
+    public void test() {
+
+    }
+
+    public void sendMail() throws MessagingException {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        Session session = Session.getInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("user", "password");
+                    }
+                });
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("mina@gmail.com"));
+        message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse("kellel@gmail.com"));
+        message.setSubject("Pealkiri");
+        message.setText("Kirja sisu");
+        Transport.send(message);
     }
 }
