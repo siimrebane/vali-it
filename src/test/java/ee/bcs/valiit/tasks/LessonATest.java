1f39c1920372429fd0326f8bf3a810017e721c67
siimrebane <siim@rebane.org>
2021-08-24 20:30:15 +0300

    tests

--- src/test/java/ee/bcs/valiit/tasks/LessonATest.java
+++ src/test/java/ee/bcs/valiit/tasks/LessonATest.java
@@ -8,16 +8,16 @@ class LessonATest {
 
     @Test
     void e1() {
-        assertEquals(-2,LessonA.e1(2));
-        assertEquals(0,LessonA.e1(0));
-        assertEquals(9,LessonA.e1(-9));
+        assertEquals(-2, LessonA.e1(2));
+        assertEquals(0, LessonA.e1(0));
+        assertEquals(9, LessonA.e1(-9));
     }
 
     @Test
     void e2() {
-        assertEquals(2, LessonA.e2(1,3));
-        assertEquals(0, LessonA.e2(-1,1));
-        assertEquals(10, LessonA.e2(0,20));
+        assertEquals(2, LessonA.e2(1, 3));
+        assertEquals(0, LessonA.e2(-1, 1));
+        assertEquals(10, LessonA.e2(0, 20));
     }
 
     @Test
@@ -36,10 +36,10 @@ class LessonATest {
     @Test
     void e5() {
         int answer = LessonA.e5();
-        assertEquals(answer%2 == 0, LessonA.e5());
-        assertEquals(answer%3 == 0, LessonA.e5());
-        assertEquals(answer%7 == 0, LessonA.e5());
-        assertEquals(answer < 55, LessonA.e5());
+        assertTrue(answer % 2 == 0);
+        assertTrue(answer % 3 == 0);
+        assertTrue(answer % 7 == 0);
+        assertTrue(answer < 55);
     }
 
     @Test
@@ -71,18 +71,18 @@ class LessonATest {
         assertTrue(LessonA.e9(false, true, false, false));
         assertFalse(LessonA.e9(true, true, true, true));
         assertFalse(LessonA.e9(false, false, false, false));
-        assertTrue(LessonA.e9(true, false, true, false));
-        assertTrue(LessonA.e9(false, true, false, true));
+        assertTrue(LessonA.e9(true, true, true, false));
+        assertTrue(LessonA.e9(false, true, true, true));
         assertFalse(LessonA.e9(true, true, false, false));
         assertFalse(LessonA.e9(false, false, true, true));
     }
 
     @Test
     void e10() {
-        int [] a = new int [] {1, 4, 6, 2, 4, 1};
-        int [] aAnswer = new int [] {2, 8, 12, 4, 8, 2};
-        int [] b = new int [] {4, 3, 1, 53, 2, 55};
-        int [] bAnswer = new int [] {8, 6, 2, 106, 4, 110};
+        int[] a = new int[]{1, 4, 6, 2, 4, 1};
+        int[] aAnswer = new int[]{2, 8, 12, 4, 8, 2};
+        int[] b = new int[]{4, 3, 1, 53, 2, 55};
+        int[] bAnswer = new int[]{8, 6, 2, 106, 4, 110};
         LessonA.e10(a);
         LessonA.e10(b);
         assertArrayEquals(aAnswer, a);
@@ -91,10 +91,10 @@ class LessonATest {
 
     @Test
     void e11() {
-        int [] a = new int [] {1, 4, 6, 2, 4, 1};
-        int [] aAnswer = new int [] {1, 0, 6, 2, 4, 1};
-        int [] b = new int [] {4, 3, 1, 53, 2, 55};
-        int [] bAnswer = new int [] {4, 0, 1, 53, 2, 55};
+        int[] a = new int[]{1, 4, 6, 2, 4, 1};
+        int[] aAnswer = new int[]{1, 0, 6, 2, 4, 1};
+        int[] b = new int[]{4, 3, 1, 53, 2, 55};
+        int[] bAnswer = new int[]{4, 0, 1, 53, 2, 55};
         LessonA.e11(a);
         LessonA.e11(b);
         assertArrayEquals(aAnswer, a);
@@ -103,10 +103,10 @@ class LessonATest {
 
     @Test
     void e12() {
-        int [] a = new int [] {1, 4, 6, 2, 4, 1};
-        int [] aAnswer = new int [] {4, 1, 6, 2, 4, 1};
-        int [] b = new int [] {4, 3, 1, 53, 2, 55};
-        int [] bAnswer = new int [] {3, 4, 1, 53, 2, 55};
+        int[] a = new int[]{1, 4, 6, 2, 4, 1};
+        int[] aAnswer = new int[]{4, 1, 6, 2, 4, 1};
+        int[] b = new int[]{4, 3, 1, 53, 2, 55};
+        int[] bAnswer = new int[]{3, 4, 1, 53, 2, 55};
         LessonA.e12(a);
         LessonA.e12(b);
         assertArrayEquals(aAnswer, a);
@@ -115,10 +115,10 @@ class LessonATest {
 
     @Test
     void e13() {
-        int [] a = new int [] {1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
-        int [] aAnswer = new int [] {1, 1, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
-        int [] b = new int [] {4, 3, 1, 53, 2, 55};
-        int [] bAnswer = new int [] {4, 4, 1, 53, 2, 55};
+        int[] a = new int[]{1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
+        int[] aAnswer = new int[]{1, 1, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
+        int[] b = new int[]{4, 3, 1, 53, 2, 55};
+        int[] bAnswer = new int[]{4, 4, 1, 53, 2, 55};
         LessonA.e13(a);
         LessonA.e13(b);
         assertArrayEquals(aAnswer, a);
@@ -127,25 +127,21 @@ class LessonATest {
 
     @Test
     void e14() {
-        int [] a = new int [] {1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
-        int [] aAnswer = new int [] {1, 1, 6, 6, 4, 4, 5, 5, 7, 7, 8 , 8};
-        int [] b = new int [] {4, 3, 1, 53, 2, 55};
-        int [] bAnswer = new int [] {4, 4, 1, 1, 2, 2};
+        int[] a = new int[]{1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
+        int[] aAnswer = new int[]{1, 1, 6, 6, 4, 4, 5, 5, 7, 1, 8, 1};
         LessonA.e14(a);
-        LessonA.e14(b);
         assertArrayEquals(aAnswer, a);
-        assertArrayEquals(bAnswer, b);
     }
 
     @Test
     void e15() {
-        int [] a = new int [] {1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1, 1};
-        int [] aAnswer = new int [] {1, 1, 6, 6, 4, 4, 5, 5, 7, 7, 8 , 8, 1};
-        int [] b = new int [] {4, 3, 1, 53, 2, 55};
-        int [] bAnswer = new int [] {4, 4, 1, 1, 2, 2};
+        int[] a = new int[]{1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1, 1};
+        int[] aAnswer = new int[]{1, 1, 6, 6, 4, 4, 5, 5, 7, 7, 8, 8, 1};
+        int[] b = new int[]{4, 3, 1, 53, 2, 55};
+        int[] bAnswer = new int[]{4, 4, 1, 1, 2, 2};
         LessonA.e15(a);
         LessonA.e15(b);
-        assertArrayEquals(a, aAnswer);
-        assertArrayEquals(b, bAnswer);
+        assertArrayEquals(aAnswer, a);
+        assertArrayEquals(bAnswer, b);
     }
 }
