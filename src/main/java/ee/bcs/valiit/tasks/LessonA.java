bfeb72376bd5aee63a6a351c0914ef8029051ef5
siimrebane <siim@rebane.org>
2021-08-24 20:06:04 +0300

    rohkem ülesandeid

---
+++ src/main/java/ee/bcs/valiit/tasks/LessonA.java
@@ -0,0 +1,71 @@
+package ee.bcs.valiit.tasks;
+
+public class LessonA {
+    public static int e1(int a){
+        // tagasta vastasmärgiline arv
+        return 0;
+    }
+    public static int e2(int a, int b){
+        // tagasta kahe arvu keskmine
+        return 0;
+    }
+    public static int e3(int x){
+        // lahuta sisendist 5 ja siis korruta 99
+        return 0;
+    }
+    public static int e4(int a1, int b1, int a2, int b2, int a3, int b3){
+       // korruta a1 b1-ga, a2 b2-ga jne. Ning siis liida saadud numbrid
+        return 0;
+    }
+    public static int e5(){
+        // return the answer to the Life, the Universe, and Everything.
+        return 0;
+    }
+    public static boolean e6(int x){
+        // Kas arv on liigaasta
+        // Wikipeediast:
+        // Iga aasta, mis jagub neljaga, on liigaasta (kui ta samal ajal ei jagu sajaga). Kui aasta jagub sajaga ja ei jagu neljasajaga siis ta ei ole liigaasta. Aasta, mis jagub neljasajaga, on alati liigaasta.
+        //See tähendab näiteks, et aastad 1984 ja 2000 olid liigaastad, 1900 aga mitte.
+        return true;
+    }
+    public static boolean e7(boolean x){
+        // tagasta x-i vastand väärtus
+        return false;
+    }
+    public static boolean e8(boolean x1, boolean x2){
+        // tagasta true kui ainult 1 sisend muutujatest on true
+        return false;
+    }
+    public static boolean e9(boolean x1, boolean x2, boolean x3, boolean x4){
+        // tagasta true kui paaritu arv sisend muutujatest on true
+        return false;
+    }
+    public static void e10(int x[]){
+        // muuda sisend massiivi nii et kõik elemendid oleksid 2x suuremad
+    }
+
+    public static void e11(int x[]){
+        // määra sisend massiivi teine element (index 1) 0-iks
+    }
+
+    public static void e12(int x[]){
+        // vaheta massiivi esimene ja teine element omavahel
+    }
+
+    public static int[] e13(int x[]){
+        // määra massiivi teise elemendi väärtuseks sama mis esimesel elemendil
+        return null;
+    }
+
+    public static void e14(int x[]){
+        // määra massiivi teise elemendi väärtuseks sama mis esimesel elemendil
+        // määra massiivi neljanda elemendi väärtuseks sama mis kolmandal elemendil
+        // määra massiivi kuuenda elemendi väärtuseks sama mis viiendal elemendil
+        // määra massiivi kaheksanda elemendi väärtuseks sama mis seitsmendal elemendil
+    }
+
+    public static void e15(int x[]){
+        // määra iga teine (indeksid 1, 3, jne) element massiivis samaks, mis oli talle eelnenud elemendi väärtus
+    }
+
+}
---
+++ src/test/java/ee/bcs/valiit/tasks/LessonATest.java
@@ -0,0 +1,151 @@
+package ee.bcs.valiit.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LessonATest {
+
+    @Test
+    void e1() {
+        assertEquals(-2,LessonA.e1(2));
+        assertEquals(0,LessonA.e1(0));
+        assertEquals(9,LessonA.e1(-9));
+    }
+
+    @Test
+    void e2() {
+        assertEquals(2, LessonA.e2(1,3));
+        assertEquals(0, LessonA.e2(-1,1));
+        assertEquals(10, LessonA.e2(0,20));
+    }
+
+    @Test
+    void e3() {
+        assertEquals(-990, LessonA.e3(-5));
+        assertEquals(-495, LessonA.e3(0));
+        assertEquals(0, LessonA.e3(5));
+    }
+
+    @Test
+    void e4() {
+        assertEquals(41, LessonA.e4(1, 3, 2, 4, 5, 6));
+        assertEquals(-12, LessonA.e4(-5, 9, 2, 18, 1, -3));
+    }
+
+    @Test
+    void e5() {
+        int answer = LessonA.e5();
+        assertEquals(answer%2 == 0, LessonA.e5());
+        assertEquals(answer%3 == 0, LessonA.e5());
+        assertEquals(answer%7 == 0, LessonA.e5());
+        assertEquals(answer < 55, LessonA.e5());
+    }
+
+    @Test
+    void e6() {
+        assertEquals(true, LessonA.e6(1984));
+        assertEquals(true, LessonA.e6(2000));
+        assertEquals(false, LessonA.e6(1900));
+        assertEquals(false, LessonA.e6(2021));
+        assertEquals(true, LessonA.e6(2020));
+    }
+
+    @Test
+    void e7() {
+        assertTrue(LessonA.e7(false));
+        assertFalse(LessonA.e7(true));
+    }
+
+    @Test
+    void e8() {
+        assertTrue(LessonA.e8(true, false));
+        assertTrue(LessonA.e8(false, true));
+        assertFalse(LessonA.e8(true, true));
+        assertFalse(LessonA.e8(false, false));
+    }
+
+    @Test
+    void e9() {
+        assertTrue(LessonA.e9(true, false, false, false));
+        assertTrue(LessonA.e9(false, true, false, false));
+        assertFalse(LessonA.e9(true, true, true, true));
+        assertFalse(LessonA.e9(false, false, false, false));
+        assertTrue(LessonA.e9(true, false, true, false));
+        assertTrue(LessonA.e9(false, true, false, true));
+        assertFalse(LessonA.e9(true, true, false, false));
+        assertFalse(LessonA.e9(false, false, true, true));
+    }
+
+    @Test
+    void e10() {
+        int [] a = new int [] {1, 4, 6, 2, 4, 1};
+        int [] aAnswer = new int [] {2, 8, 12, 4, 8, 2};
+        int [] b = new int [] {4, 3, 1, 53, 2, 55};
+        int [] bAnswer = new int [] {8, 6, 2, 106, 4, 110};
+        LessonA.e10(a);
+        LessonA.e10(b);
+        assertArrayEquals(aAnswer, a);
+        assertArrayEquals(bAnswer, b);
+    }
+
+    @Test
+    void e11() {
+        int [] a = new int [] {1, 4, 6, 2, 4, 1};
+        int [] aAnswer = new int [] {1, 0, 6, 2, 4, 1};
+        int [] b = new int [] {4, 3, 1, 53, 2, 55};
+        int [] bAnswer = new int [] {4, 0, 1, 53, 2, 55};
+        LessonA.e11(a);
+        LessonA.e11(b);
+        assertArrayEquals(aAnswer, a);
+        assertArrayEquals(bAnswer, b);
+    }
+
+    @Test
+    void e12() {
+        int [] a = new int [] {1, 4, 6, 2, 4, 1};
+        int [] aAnswer = new int [] {4, 1, 6, 2, 4, 1};
+        int [] b = new int [] {4, 3, 1, 53, 2, 55};
+        int [] bAnswer = new int [] {3, 4, 1, 53, 2, 55};
+        LessonA.e12(a);
+        LessonA.e12(b);
+        assertArrayEquals(aAnswer, a);
+        assertArrayEquals(bAnswer, b);
+    }
+
+    @Test
+    void e13() {
+        int [] a = new int [] {1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
+        int [] aAnswer = new int [] {1, 1, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
+        int [] b = new int [] {4, 3, 1, 53, 2, 55};
+        int [] bAnswer = new int [] {4, 4, 1, 53, 2, 55};
+        LessonA.e13(a);
+        LessonA.e13(b);
+        assertArrayEquals(aAnswer, a);
+        assertArrayEquals(bAnswer, b);
+    }
+
+    @Test
+    void e14() {
+        int [] a = new int [] {1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1};
+        int [] aAnswer = new int [] {1, 1, 6, 6, 4, 4, 5, 5, 7, 7, 8 , 8};
+        int [] b = new int [] {4, 3, 1, 53, 2, 55};
+        int [] bAnswer = new int [] {4, 4, 1, 1, 2, 2};
+        LessonA.e14(a);
+        LessonA.e14(b);
+        assertArrayEquals(aAnswer, a);
+        assertArrayEquals(bAnswer, b);
+    }
+
+    @Test
+    void e15() {
+        int [] a = new int [] {1, 4, 6, 2, 4, 1, 5, 2, 7, 1, 8, 1, 1};
+        int [] aAnswer = new int [] {1, 1, 6, 6, 4, 4, 5, 5, 7, 7, 8 , 8, 1};
+        int [] b = new int [] {4, 3, 1, 53, 2, 55};
+        int [] bAnswer = new int [] {4, 4, 1, 1, 2, 2};
+        LessonA.e15(a);
+        LessonA.e15(b);
+        assertArrayEquals(a, aAnswer);
+        assertArrayEquals(b, bAnswer);
+    }
+}
