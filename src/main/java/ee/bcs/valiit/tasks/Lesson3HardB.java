a57df6f9d7f9ff4732ba38f33e00602bd2a770d0
Siim Rebane <siim@rebane.org>
2021-09-01 08:59:03 +0300

    Uus ülesanne

---
+++ src/main/java/ee/bcs/valiit/tasks/Lesson3HardB.java
@@ -0,0 +1,15 @@
+package ee.bcs.valiit.tasks;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson3HardB {
+
+    // TODO kirjuta mäng kus kasutaja peab ära arvama numbri 0-99 (nagu 3Hard)
+    // NB programm ei tohi kohe alguses välja mõelda numbrit
+    // vaid eesmärk on öelda kasutajale, et ta eksis nii kaua kui võimalik
+    // ilma selleta, et ta läheks vastuollu ühegi varasema väitega
+    public static void main(String[] args){
+
+    }
+}
