5b0c8af5fb8615b63ebfd57bde6fcd64a0245669
Siim Rebane <siim@rebane.org>
2021-11-07 16:23:28 +0200

    fix

--- src/main/java/ee/bcs/valiit/sample/ScheduledTasks.java
+++ src/main/java/ee/bcs/valiit/sample/ScheduledTasks.java
@@ -1,6 +1,5 @@
 package ee.bcs.valiit.sample;
 
-import ee.bcs.valiit.controller.model.SampleEmployeeDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,11 +12,12 @@ public class ScheduledTasks {
 
     @Scheduled(fixedDelay = 5000)
     public void test(){
+        /*
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<SampleEmployeeDto[]> a = restTemplate.getForEntity("http://localhost:8080/sql-test4",
                 SampleEmployeeDto[].class);
         System.out.println(Arrays.toString(a.getBody()));
         ResponseEntity<String> b = restTemplate.getForEntity("http://localhost:8080/sql-test4", String.class);
-        System.out.println(b.getBody());
+        System.out.println(b.getBody());*/
     }
 }
