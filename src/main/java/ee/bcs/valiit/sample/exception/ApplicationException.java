5ee2e0ba848933b94f3b0d64ba34064c4d58ebac
Siim Rebane <siim@rebane.org>
2021-11-08 11:24:35 +0200

    remove not neede files

--- src/main/java/ee/bcs/valiit/sample/exception/ApplicationException.java
+++
@@ -1,7 +0,0 @@
-package ee.bcs.valiit.sample.exception;
-
-public class ApplicationException extends RuntimeException {
-    public ApplicationException(String message) {
-        super(message);
-    }
-}
