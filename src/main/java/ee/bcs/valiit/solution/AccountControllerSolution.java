4aae7fa3b711c86d574a129845253876dc4ad232
Siim Rebane <siim@rebane.org>
2021-11-19 09:46:15 +0200

    RowMapper samples

--- src/main/java/ee/bcs/valiit/solution/AccountControllerSolution.java
+++ src/main/java/ee/bcs/valiit/solution/AccountControllerSolution.java
@@ -2,23 +2,28 @@ package ee.bcs.valiit.solution;
 
 import ee.bcs.valiit.controller.SolutionDepositMoneyRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class AccountControllerSolution {
     @Autowired
-    private NamedParameterJdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @PostMapping("solution/account/{account}")
     public String createAccount(@PathVariable("account") String accountNumber){
         String sql = "INSERT INTO account (account_number) VALUES (:accountNumber)";
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("accountNumber", accountNumber);
-        jdbcTemplate.update(sql, paramMap);
+        namedParameterJdbcTemplate.update(sql, paramMap);
         return "Konto " + accountNumber + " on loodud";
     }
 
@@ -27,7 +32,7 @@ public class AccountControllerSolution {
         String sql = "SELECT balance FROM account WHERE account_number = :accountNumber";
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("accountNumber", accountNumber);
-        int balance = jdbcTemplate.queryForObject(sql, paramMap, Integer.class);
+        int balance = namedParameterJdbcTemplate.queryForObject(sql, paramMap, Integer.class);
         return "Konto " + accountNumber + " balanss on: " + balance;
     }
 
@@ -37,13 +42,13 @@ public class AccountControllerSolution {
         String sql = "SELECT balance FROM account WHERE account_number = :accountNumber";
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("accountNumber", accountNumber);
-        int balance = jdbcTemplate.queryForObject(sql, paramMap, Integer.class);
+        int balance = namedParameterJdbcTemplate.queryForObject(sql, paramMap, Integer.class);
         int newBalance = balance + amount;
         String sql2 = "UPDATE account SET balance = :newBalance WHERE account_number = :accountNumber";
         Map<String, Object> paramMap2 = new HashMap<>();
         paramMap2.put("accountNumber", accountNumber);
         paramMap2.put("newBalance", newBalance);
-        jdbcTemplate.update(sql2, paramMap2);
+        namedParameterJdbcTemplate.update(sql2, paramMap2);
         return "Konto " + accountNumber + " uus balanss on: " + newBalance;
     }
 
@@ -52,13 +57,56 @@ public class AccountControllerSolution {
         String sql = "SELECT balance FROM account WHERE account_number = :accountNumber";
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("accountNumber", request.getAccountNumber());
-        int balance = jdbcTemplate.queryForObject(sql, paramMap, Integer.class);
+        int balance = namedParameterJdbcTemplate.queryForObject(sql, paramMap, Integer.class);
         int newBalance = balance + request.getAmount();
         String sql2 = "UPDATE account SET balance = :newBalance WHERE account_number = :accountNumber";
         Map<String, Object> paramMap2 = new HashMap<>();
         paramMap2.put("accountNumber", request.getAccountNumber());
         paramMap2.put("newBalance", newBalance);
-        jdbcTemplate.update(sql2, paramMap2);
+        namedParameterJdbcTemplate.update(sql2, paramMap2);
         return "Konto " + request.getAccountNumber() + " uus balanss on: " + newBalance;
     }
+
+    @GetMapping("solution/account")
+    public List<AccountDtoSolution> getAllAccounts(){
+        String sql = "SELECT * FROM account";
+        Map<String, Object> paraMap = new HashMap<>();
+        List<AccountDtoSolution> result = namedParameterJdbcTemplate.query(sql, paraMap, new BeanPropertyRowMapper<>(AccountDtoSolution.class));
+        return result;
+    }
+
+    @GetMapping("solution/account2")
+    public List<AccountDtoSolution> getAllAccounts2(){
+        String sql = "SELECT * FROM account";
+        Map<String, Object> paraMap = new HashMap<>();
+        List<AccountDtoSolution> result = namedParameterJdbcTemplate.query(sql, paraMap, new AccoutDtoRowMapper());
+        return result;
+    }
+
+    private class AccoutDtoRowMapper implements RowMapper<AccountDtoSolution> {
+        @Override
+        public AccountDtoSolution mapRow(ResultSet resultSet, int i) throws SQLException {
+            AccountDtoSolution result = new AccountDtoSolution();
+            result.setAccountNumber(resultSet.getString("account_number"));
+            result.setBalance(resultSet.getInt("balance"));
+            result.setLocked(resultSet.getBoolean("locked"));
+            return result;
+        }
+    }
+
+    @GetMapping("solution/account2/{id}")
+    public AccountDtoSolution getAccountInfo(@PathVariable("id") String accountNumber){
+        String sql = "SELECT * FROM account WHERE account_number = :accountNumber";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("accountNumber", accountNumber);
+        AccountDtoSolution accountDto = namedParameterJdbcTemplate.queryForObject(sql, paramMap, new AccoutDtoRowMapper());
+        if (accountDto.isLocked()) {
+            System.out.println("konto on lukus");
+            return null;
+        }
+        System.out.println("konto jääk on: " + accountDto.getBalance());
+        return accountDto;
+    }
+
+    // TODO 1 - teenuse, mis tagastab kogu info kõigi kontode kohta
 }
---
+++ src/main/java/ee/bcs/valiit/solution/AccountDtoSolution.java
@@ -0,0 +1,31 @@
+package ee.bcs.valiit.solution;
+
+public class AccountDtoSolution {
+    private String accountNumber;
+    private Integer balance;
+    private boolean locked;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+}
