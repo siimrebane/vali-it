dd4dc8fa31b0278f4aefde3d39b0fc420f783b55
Siim Rebane <siim@rebane.org>
2021-11-19 13:05:49 +0200

    Lesson6

---
+++ src/main/java/ee/bcs/valiit/tasks/Lesson6Controller.java
@@ -0,0 +1,11 @@
+package ee.bcs.valiit.tasks;
+
+public class Lesson6Controller {
+    // Don't write code here (update your existing Lesson4Controller)
+    // Add extra service
+    // getTransactionHistory
+    // Input: accountNumber
+    // Output: List of all transactions related to this account
+    // (deposit, withdraw, transfer-from, transfer-to)
+    // HINT: you have to do some changes in depositMoney, withdrawMoney and transferMoney as well
+}
